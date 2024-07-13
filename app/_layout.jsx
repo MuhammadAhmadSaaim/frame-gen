df7a0fbc363a07de9da8a21e05d93f6d3ab1ae81
Ahmad <131032658+MuhammadAhmadSaaim@users.noreply.github.com>
2024-07-13 12:58:48 +0500

    added tailwindcss

--- app/_layout.jsx
+++ app/_layout.jsx
@@ -1,20 +1,15 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react'
+import { Slot, Stack } from "expo-router"
+import { StyleSheet, Text, View } from 'react-native'
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>FrameGen</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+const RootLayout = () => {
+    {
+        return (
+            <Stack>
+                <Stack.Screen name="index" options={{ headerShown: false }} />
+            </Stack>
+        )
+    }
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+export default RootLayout
